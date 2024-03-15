58d9e0fd34b5084ab831cf7fb427c483108c1ee3
Maciej Kowalski <kowalski.exe@gmail.com>
2024-03-15 22:16:52 +0100

    Potyczki algorytmiczne dzień 3 C 7/10 faster

--- Potyczki Algorytmiczne/2024/Dzień 4/Łamigłówka 3 [C]/lam.cpp
+++ Potyczki Algorytmiczne/2024/Dzień 4/Łamigłówka 3 [C]/lam.cpp
@@ -1,4 +1,4 @@
-// 7/10
+// 7/10 faster
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -10,33 +10,35 @@ typedef long long ll;
 #define DOUT if(0) cout
 #endif
 
-enum Dir {
-    row,
-    column
-};
-
-bool check(const vector<string> &canvas, Dir dir, int nr, char &color) {
-    int y = canvas.size();
+bool check_row(const vector<string> &canvas, int nr, char &color) {
     int x = canvas[0].size();
-    int limit;
-    if (dir == Dir::row)
-        limit = x;
-    else
-        limit = y;
     color = '*';
     bool succ = true;
-    for (int i = 0; i < limit; i++) {
-        int cell;
-        if (dir == Dir::row)
-            cell = canvas[nr][i];
-        else
-            cell = canvas[i][nr];
-
-        if (cell != '*') {
+    for (int i = 0; i < x; i++) {
+        if (canvas[nr][i] != '*') {
             if (color == '*')
-                color = cell;
+                color = canvas[nr][i];
             else {
-                if (cell != color) {
+                if (canvas[nr][i] != color) {
+                    succ = false;
+                    break;
+                }
+            }
+        }
+    }
+    return succ && color != '*';
+}
+
+bool check_column(const vector<string> &canvas, int nr, char &color) {
+    int y = canvas.size();
+    color = '*';
+    bool succ = true;
+    for (int i = 0; i < y; i++) {
+        if (canvas[i][nr] != '*') {
+            if (color == '*')
+                color = canvas[i][nr];
+            else {
+                if (canvas[i][nr] != color) {
                     succ = false;
                     break;
                 }
@@ -52,53 +54,68 @@ struct Match {
     Match(char dir, char color, int nr): dir(dir), color(color), nr(nr) {}
 };
 
-vector<Match> check_axis(vector<string> &canvas, Dir dir) {
+void check_all_rows(
+    vector<string> &canvas,
+    vector<Match> &matches,
+    vector<bool> &skip
+) {
     int y = canvas.size();
     int x = canvas[0].size();
-    int limit;
-    if (dir == Dir::row)
-        limit = y; // number of rows
-    else
-        limit = x; // number of columns
 
-    vector<Match> matches;
-    for (int i = 0; i < limit; i++) {
+    for (int i = 0; i < y; i++) {
+        // if (skip[i])
+        //     continue;
         char color;
-        bool found = check(canvas, dir, i, color);
+        bool found = check_row(canvas, i, color);
         if (found) {
-            if (dir == Dir::row)
-                matches.emplace_back('R', color, i + 1);
-            else
-                matches.emplace_back('K', color, i + 1);
+            matches.emplace_back('R', color, i + 1);
+            // skip[i] = true;
 
-            if (dir == Dir::row) {
-                DOUT << "row ";
-            } else {
-                DOUT << "column ";
-            }
-            DOUT << i + 1 << " " << color << "\n";
+            DOUT << "row " << i + 1 << " " << color << "\n";
 
-            if (dir == Dir::row) {
-                for (int j = 0; j < x; j++)
-                    canvas[i][j] = '*';
-            } else {
-                for (int j = 0; j < y; j++)
-                    canvas[j][i] = '*';
-            }
+            for (int j = 0; j < x; j++)
+                canvas[i][j] = '*';
         }
     }
-    return matches;
 }
 
-void test_check();
-void test_check_axis();
+void check_all_columns(
+    vector<string> &canvas,
+    vector<Match> &matches,
+    vector<bool> &skip
+) {
+    int y = canvas.size();
+    int x = canvas[0].size();
+
+    for (int i = 0; i < x; i++) {
+        // if (skip[i])
+        //     continue;
+        char color;
+        bool found = check_column(canvas, i, color);
+        if (found) {
+            matches.emplace_back('K', color, i + 1);
+            // skip[i] = true;
+
+            DOUT << "column " << i + 1 << " " << color << "\n";
+
+            for (int j = 0; j < y; j++)
+                canvas[j][i] = '*';
+        }
+    }
+}
+
+void test_check_row();
+void test_check_column();
+void test_check_all_rows();
+void test_check_all_columns();
 
 int main() {
 #if DEBUG
-    test_check();
-    test_check_axis();
-#endif
-#if !defined(DEBUG)
+    test_check_row();
+    test_check_column();
+    test_check_all_rows();
+    test_check_all_columns();
+#else
     ios_base::sync_with_stdio(false);
 #endif
     int y, x;
@@ -108,38 +125,33 @@ int main() {
         cin >> canvas[i];
     vector<Match> matches;
     bool found_anything;
+    vector<bool> skip_columns(x, false);
+    vector<bool> skip_rows(y, false);
     do {
         found_anything = false;
 
-        // check all columns
-        auto tmp = check_axis(canvas, Dir::column);
-        if (tmp.size() > 0)
+        int before = matches.size();
+        check_all_columns(canvas, matches, skip_columns);
+        int after = matches.size();
+        if (after > before)
             found_anything = true;
-        matches.insert(
-            matches.end(),
-            make_move_iterator(tmp.begin()),
-            make_move_iterator(tmp.end())
-        );
         DOUT << "after checking columns\n";
-        for (const Match &m : matches)
-            DOUT << m.dir << " " << m.nr << " " << m.color;
+        for (int i = before; i < after; i++)
+            DOUT << matches[i].dir << " " << matches[i].nr << " " << matches[i].color << "\n";
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++)
                 DOUT << canvas[i][j];
             DOUT << "\n";
         }
-        // check all rows
-        tmp = check_axis(canvas, Dir::row);
-        if (tmp.size() > 0)
+
+        before = matches.size();
+        check_all_rows(canvas, matches, skip_rows);
+        after = matches.size();
+        if (after > before)
             found_anything = true;
-        matches.insert(
-            matches.end(),
-            make_move_iterator(tmp.begin()),
-            make_move_iterator(tmp.end())
-        );
         DOUT << "after checking rows\n";
-        for (const Match &m : matches)
-            DOUT << m.dir << " " <<  m.nr << " " <<  m.color << "\n";
+        for (int i = before; i < after; i++)
+            DOUT << matches[i].dir << " " << matches[i].nr << " " << matches[i].color << "\n";
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++)
                 DOUT << canvas[i][j];
@@ -152,7 +164,7 @@ int main() {
         cout << m.dir << " " << m.nr << " " << m .color << "\n";
 }
 
-void test_check() {
+void test_check_row() {
     vector<string> canvas = {
         "AAA*",
         "A*B*",
@@ -161,51 +173,66 @@ void test_check() {
     };
     // check row 0
     char color;
-    bool found = check(canvas, Dir::row, 0, color);
+    bool found = check_row(canvas, 0, color);
     assert(found);
     assert(color == 'A');
 
     // check row 1
-    found = check(canvas, Dir::row, 1, color);
+    found = check_row(canvas, 1, color);
     assert(!found);
 
     // check row 2
-    found = check(canvas, Dir::row, 2, color);
+    found = check_row(canvas, 2, color);
     assert(!found);
 
     // check row 3
-    found = check(canvas, Dir::row, 3, color);
+    found = check_row(canvas, 3, color);
     assert(found);
     assert(color == 'B');
 
+    DOUT << "test_check_row passed\n";
+}
+
+void test_check_column() {
+    vector<string> canvas = {
+        "AAA*",
+        "A*B*",
+        "****",
+        "B*B*"
+    };
+    bool found;
+    char color;
+
     // check column 1
-    found = check(canvas, Dir::column, 0, color);
+    found = check_column(canvas, 0, color);
     assert(!found);
 
     // check column 2
-    found = check(canvas, Dir::column, 1, color);
+    found = check_column(canvas, 1, color);
     assert(found);
     assert(color == 'A');
 
     // check column 3
-    found = check(canvas, Dir::column, 2, color);
+    found = check_column(canvas, 2, color);
     assert(!found);
 
     // check column 4
-    found = check(canvas, Dir::column, 3, color);
+    found = check_column(canvas, 3, color);
     assert(!found);
 
-    DOUT << "test_check passed\n";
+    DOUT << "test_check_column passed\n";
 }
 
-void test_check_axis() {
+void test_check_all_columns() {
     vector<string> canvas = {
-        "AAAA",
-        "A*BA",
+        "AAA*",
+        "A*B*",
         "****",
-        "AAAA"
+        "AAA*"
     };
-    check_axis(canvas, Dir::column);
+    vector<Match> matches;
+    vector<bool> skip(4, false);
+    check_all_columns(canvas, matches, skip);
     // K 1 A
     assert(canvas[0][0] == '*');
     assert(canvas[1][0] == '*');
@@ -216,28 +243,34 @@ void test_check_axis() {
     assert(canvas[1][1] == '*');
     assert(canvas[2][1] == '*');
     assert(canvas[3][1] == '*');
-    // K 4 A
-    assert(canvas[0][3] == '*');
-    assert(canvas[1][3] == '*');
-    assert(canvas[2][3] == '*');
-    assert(canvas[3][3] == '*');
 
-    check_axis(canvas, Dir::row);
+    assert((int)matches.size() == 2);
+
+    DOUT << "test_check_all_columns passed\n";
+}
+
+void test_check_all_rows() {
+    vector<string> canvas = {
+        "A*AA",
+        "A*BA",
+        "****",
+        "AAAA"
+    };
+    vector<Match> matches;
+    vector<bool> skip(4, false);
+    check_all_rows(canvas, matches, skip);
     // R 1 A
     assert(canvas[0][0] == '*');
     assert(canvas[0][1] == '*');
     assert(canvas[0][2] == '*');
     assert(canvas[0][3] == '*');
-    // R 2 B
-    assert(canvas[1][0] == '*');
-    assert(canvas[1][1] == '*');
-    assert(canvas[1][2] == '*');
-    assert(canvas[1][3] == '*');
     // R 3 A
     assert(canvas[3][0] == '*');
     assert(canvas[3][1] == '*');
     assert(canvas[3][2] == '*');
     assert(canvas[3][3] == '*');
 
-    DOUT << "test_check_axis passed\n";
+    assert((int)matches.size() == 2);
+
+    DOUT << "test_check_all_rows passed\n";
 }
